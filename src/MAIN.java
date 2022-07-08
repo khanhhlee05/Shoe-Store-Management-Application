356d0d0919245b53f28ebc5877b73c1a7a9b17a3
ADMIN <ADMIN@192.168.1.22>
2022-07-08 15:17:02 +0700

    CS_IA_Khanh

---
+++ src/MAIN.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class MAIN {
+    public static void main(String[] args){
+        new Login().setVisible(true);
+    }
+    
+}
